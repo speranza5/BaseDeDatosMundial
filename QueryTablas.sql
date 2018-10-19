a080b1b61407cf0b54b33295f83ea9bdd369fa17
Carlos Speranza <carlossperanza5@gmail.com>
2018-10-19 17:00:51 -0300

    Puse equipo1 y equipo 2

--- QueryTablas.sql
+++ QueryTablas.sql
@@ -47,17 +47,6 @@ CREATE TABLE Estadio(
 	codigo_ciudad int NOT NULL FOREIGN KEY REFERENCES Ciudad(codigo_ciudad)
 );
 
-CREATE TABLE Partido(
-	anio_mundial int NOT NULL FOREIGN KEY REFERENCES Mundial(anio_mundial),
-	codigo_instancia int NOT NULL FOREIGN KEY REFERENCES Instancia(codigo_instancia),
-	codigo_grupo int FOREIGN KEY REFERENCES Grupo(codigo_grupo),
-	codigo_estadio int NOT NULL FOREIGN KEY REFERENCES Estadio(codigo_estadio),
-	numero_partido int NOT NULL IDENTITY(1,1),
-	fecha_hora smalldatetime NOT NULL,
-	PRIMARY KEY(anio_mundial,numero_partido)
-);
-
-
 CREATE TABLE Equipo(
 	codigo_pais int NOT NULL FOREIGN KEY REFERENCES Pais(codigo_pais),
 	anio_mundial int NOT NULL FOREIGN KEY REFERENCES Mundial(anio_mundial),
@@ -66,6 +55,28 @@ CREATE TABLE Equipo(
 	cant_amarillas int,
 	PRIMARY KEY(codigo_pais,anio_mundial)
 );
+
+CREATE TABLE Partido(
+	anio_mundial int NOT NULL FOREIGN KEY REFERENCES Mundial(anio_mundial),
+	codigo_instancia int NOT NULL FOREIGN KEY REFERENCES Instancia(codigo_instancia),
+	codigo_grupo int FOREIGN KEY REFERENCES Grupo(codigo_grupo),
+	codigo_estadio int NOT NULL FOREIGN KEY REFERENCES Estadio(codigo_estadio),
+	codigo_pais_1 int NOT NULL,
+	anio_mundial_1 int NOT NULL,
+
+	codigo_pais_2 int NOT NULL,
+	anio_mundial_2 int NOT NULL,
+
+	CONSTRAINT equipo_1 FOREIGN KEY(codigo_pais_1, anio_mundial_1) REFERENCES Equipo(codigo_pais,anio_mundial),
+	CONSTRAINT equipo_2 FOREIGN KEY(codigo_pais_2, anio_mundial_2) REFERENCES Equipo(codigo_pais,anio_mundial),
+	numero_partido int NOT NULL IDENTITY(1,1),
+	fecha_hora smalldatetime NOT NULL,
+
+	PRIMARY KEY(anio_mundial,numero_partido)
+);
+
+
+
 
 CREATE TABLE Aloja(
 	codigo_pais int NOT NULL FOREIGN KEY REFERENCES Pais(codigo_pais),
