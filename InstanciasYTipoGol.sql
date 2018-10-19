65d3a26dcd2a835b9ba89eb6389c6768bd8fae7d
Carlos Speranza <carlossperanza5@gmail.com>
2018-10-19 17:57:50 -0300

    Subo las instancias y tipos de gol

---
+++ InstanciasYTipoGol.sql
@@ -0,0 +1,5 @@
+INSERT INTO Tipo_Gol (descripcion)
+VALUES ('jugada colectiva'),('cabeza'),('tiro libre'),('penal'),('Corner'),('Chilena'),('Taco'),('Rabona'),('Con la Mano'),('jugada personal'),('otros');
+
+INSERT INTO Instancia (codigo_instancia,nombre)
+VALUES (1,'fase de grupos'),(2,'octavos de fina'),(3,'cuartos de final'),(4,'semifinal'),(5,'tercer puesto'),(6,'final');
