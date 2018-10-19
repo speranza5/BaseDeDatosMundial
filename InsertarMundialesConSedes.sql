3253c5d80412719e45e819307426771949a61a3a
Carlos Speranza <carlossperanza5@gmail.com>
2018-10-19 17:43:32 -0300

    Sedes de los mundiales

---
+++ InsertarMundialesConSedes.sql
@@ -0,0 +1,4 @@
+INSERT INTO Mundial (anio_mundial)
+VALUES (2018),(2014),(2010),(2006),(2002);
+
+INSERT INTO Aloja (codigo_pais,anio_mundial) VALUES (76,2018),(30,2014),(80,2010),(5,2006),(43,2002),(62,2002);
