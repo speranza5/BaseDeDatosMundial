b983a45ca03f40e1dc2d9d0c6ae78e7e3e951221
Carlos Speranza <carlossperanza5@gmail.com>
2018-10-19 19:03:47 -0300

    God lo que tarde con esta mierda

---
+++ CiudadesYEstadios.sql
@@ -0,0 +1,16 @@
+INSERT INTO Ciudad (nombre) 
+VALUES ('Moscu'),('San Petesburgo'),('Sochi'),('Volgogrado'),('Kaliningrado'),('Kazan'),('Samara'),('Nizhni Novgorod'),('Saransk'),('Rostov on Don'),('Ekaterimburgo'),
+		('Belo Horizonte'),('Brasilia'),('Cuiaba'),('Curitiba'),('Fortaleza'),('Manaos'),('Natal'),('Porto Alegre'), ('Recife'),('Rio de Janeiro'),('Salvador'),('San Pablo'),
+		('Cape Town'),('Polokwane'),('Bloemfontein'),('Port Elizabeth'),('Durban'),('Nelspruit'),('Pretoria'),('Rustenburg'),('Johannesburgo'),
+		('Berlin'),('Dortmund'),('Munich'),('Stuttgart'),('Gelsenkirchen'),('Hamburgo'),('Frankfurt'),('Colonia'),('Hannover'),('Lipzig'),('Kaiserslautern'),('Nuremberg'),
+		('Seul'),('Daegu'),('Busan'),('Incheon'),('Ulsan'),('Suwon'),('Gwangju'),('Jeonju'),('Daejeon'),('Seogwipo'),('Yokohama'),('Shizuoka'),('Osaka'),('Miyagi'),('Oita'),('Niigata'),
+		('Kashima'),('Kobe'),('Saporo');
+
+INSERT INTO Estadio (codigo_estadio,nombre_Estadio,codigo_ciudad)
+VALUES (11,'Luzhniki Stadium',1),(21,'Spartak Stadium',1),(31,'Estadio de San Petersburgo',2),(41,'Fisht',3),(51,'Volgogrado Arena',4),(61,'Estadio de Kaliningrado',5),(71,'Kazan Arena',6),(81,'Samara Arena',7),(91,'Estadio de N. Novgorod',8),
+		(101,'Mordovia Arena',9),(111,'Rostov Arena',10),(121,'Ekaterimburgo Arena',11),
+		(12,'Mineirao',12),(22,'Mane Garrincha',13),(32,'Pantanal',14),(42,'da Baixada',15),(52,'Castelao',16),(62,'Arena da Amazonia',17),(72,'das Dunas',18),(82,'Beira Rio',19),(92,'Arena Pernambuco',20),(102,'Maracana',21),(112,'Fonte Nova',22),(122,'Arena Corinthians',23),
+		(13,'Green Point',24),(23,'Peter Mokaba',25),(33,'Free State',26),(43,'Nelson Mandela Bay',27),(53,'Moses Mahbida',28),(63,'Mbombela',29),(73,'Loftus Versfield',30),(83,'Bafokeng',31),(93,'Soccer city',32),(103,'Ellis Park',33),
+		(14,'Estadio Olimpico',34),(24,'Signal Iduna Park',35),(34,'Allianz Arena',36),(44,'Gottlieb-Daimler-Stadion',37),(54,'Veltins',38),(64,'AOL Arena',39),(74,'Commerzbank',40),(84,'RheinEnergieStadion',41),(94,'AWD-Arena',42),(104,'Zentralstadion',43),(114,'Fritz-Walter-Stadion',44),(124,'Frankenstadion',45),
+		(15,'Mundialista de Seul',46),(25,'Daegu',47),(35,'Asiad',48),(45,'Munhak',49),(55,'Mundialista de Ulsan',50),(65,'Mundialista de Suwon',51),(75,'Mundialista de Gwangju',52),(85,'Mundialista de Jeonju',53),(95,'Mundialista de Daejeon',55),(105,'Mundialista de Jeju',56),
+		(16,'Internacional',57),(26,'Saitama',57),(36,'Ecopa',58),(46,'Nagai',59),(56,'Miyagi',60),(66,'Oita',62),(76,'Niigata',63),(86,'Kashima',64),(96,'de las Alas',65),(106,'Sapporo Dome',66);
