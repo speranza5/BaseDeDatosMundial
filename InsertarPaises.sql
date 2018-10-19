a0f5703f81e17be7b9a813816ef78231a52bb17a
Carlos Speranza <carlossperanza5@gmail.com>
2018-10-19 17:30:32 -0300

    Calate esa de paises wacho

---
+++ InsertarPaises.sql
@@ -0,0 +1,108 @@
+INSERT INTO Pais (nombre) VALUES ('Afganistán');
+INSERT INTO Pais (nombre) VALUES ('Islas Gland');
+INSERT INTO Pais (nombre) VALUES ('Albania');
+INSERT INTO Pais (nombre) VALUES ('Alemania');
+INSERT INTO Pais (nombre) VALUES ('Andorra');
+INSERT INTO Pais (nombre) VALUES ('Angola');
+INSERT INTO Pais (nombre) VALUES ('Anguilla');
+
+INSERT INTO Pais (nombre) VALUES ('Antigua y Barbuda');
+
+INSERT INTO Pais (nombre) VALUES ('Arabia Saudita');
+INSERT INTO Pais (nombre) VALUES ('Argelia');
+
+INSERT INTO Pais (nombre) VALUES ('Armenia');
+INSERT INTO Pais (nombre) VALUES ('Aruba');
+INSERT INTO Pais (nombre) VALUES ('Australia');
+INSERT INTO Pais (nombre) VALUES ('Austria');
+INSERT INTO Pais (nombre) VALUES ('Azerbaiyán');
+INSERT INTO Pais (nombre) VALUES ('Bahamas');
+INSERT INTO Pais (nombre) VALUES ('Bahréin');
+INSERT INTO Pais (nombre) VALUES ('Bangladesh');
+INSERT INTO Pais (nombre) VALUES ('Barbados');
+INSERT INTO Pais (nombre) VALUES ('Bielorrusia');
+INSERT INTO Pais (nombre) VALUES ('Bélgica');
+INSERT INTO Pais (nombre) VALUES ('Belice');
+INSERT INTO Pais (nombre) VALUES ('Benin');
+INSERT INTO Pais (nombre) VALUES ('Bermudas');
+INSERT INTO Pais (nombre) VALUES ('Bhután');
+INSERT INTO Pais (nombre) VALUES ('Bolivia');
+INSERT INTO Pais (nombre) VALUES ('Bosnia y Herzegovina');
+INSERT INTO Pais (nombre) VALUES ('Botsuana');
+INSERT INTO Pais (nombre) VALUES ('Brasil');
+INSERT INTO Pais (nombre) VALUES ('Brunéi');
+INSERT INTO Pais (nombre) VALUES ('Bulgaria');
+INSERT INTO Pais (nombre) VALUES ('Burkina Faso');
+INSERT INTO Pais (nombre) VALUES ('Burundi');
+INSERT INTO Pais (nombre) VALUES ('Cabo Verde');
+
+INSERT INTO Pais (nombre) VALUES ('Camerún');
+INSERT INTO Pais (nombre) VALUES ('Canadá');
+
+
+INSERT INTO Pais (nombre) VALUES ('República Checa');
+INSERT INTO Pais (nombre) VALUES ('Chile');
+
+INSERT INTO Pais (nombre) VALUES ('Chipre');
+
+
+
+INSERT INTO Pais (nombre) VALUES ('Colombia');
+
+
+
+INSERT INTO Pais (nombre) VALUES ('Corea del Norte');
+INSERT INTO Pais (nombre) VALUES ('Corea del Sur');
+INSERT INTO Pais (nombre) VALUES ('Costa de Marfil');
+INSERT INTO Pais (nombre) VALUES ('Costa Rica');
+INSERT INTO Pais (nombre) VALUES ('Croacia');
+
+INSERT INTO Pais (nombre) VALUES ('Dinamarca');
+
+
+INSERT INTO Pais (nombre) VALUES ('Ecuador');
+INSERT INTO Pais (nombre) VALUES ('Egipto');
+
+INSERT INTO Pais (nombre) VALUES ('Emiratos Árabes Unidos');
+
+INSERT INTO Pais (nombre) VALUES ('Eslovaquia');
+INSERT INTO Pais (nombre) VALUES ( 'Eslovenia');
+INSERT INTO Pais (nombre) VALUES ('España');
+
+INSERT INTO Pais (nombre) VALUES ('Estados Unidos');
+
+INSERT INTO Pais (nombre) VALUES ('Finlandia');
+
+INSERT INTO Pais (nombre) VALUES ('Francia');
+INSERT INTO Pais (nombre) VALUES ('Grecia');
+INSERT INTO Pais (nombre) VALUES ('Honduras');
+INSERT INTO Pais (nombre) VALUES ('Irán');
+INSERT INTO Pais (nombre) VALUES ('Islandia');
+INSERT INTO Pais (nombre) VALUES ('Italia');
+INSERT INTO Pais (nombre) VALUES ('Japón');
+INSERT INTO Pais (nombre) VALUES ('Marruecos');
+INSERT INTO Pais (nombre) VALUES ('México');
+INSERT INTO Pais (nombre) VALUES ('Nicaragua');
+INSERT INTO Pais (nombre) VALUES ('Nigeria');
+INSERT INTO Pais (nombre) VALUES ( 'Noruega');
+INSERT INTO Pais (nombre) VALUES ('Nueva Zelanda');
+INSERT INTO Pais (nombre) VALUES ('Países Bajos');
+INSERT INTO Pais (nombre) VALUES ('Panamá');
+INSERT INTO Pais (nombre) VALUES ('Paraguay');
+INSERT INTO Pais (nombre) VALUES ('Perú');
+INSERT INTO Pais (nombre) VALUES ('Polonia');
+INSERT INTO Pais (nombre) VALUES ('Portugal');
+INSERT INTO Pais (nombre) VALUES ('Inglaterra');
+INSERT INTO Pais (nombre) VALUES ('Rusia');
+INSERT INTO Pais (nombre) VALUES ('Senegal');
+INSERT INTO Pais (nombre) VALUES ( 'Serbia y Montenegro');
+INSERT INTO Pais (nombre) VALUES ('Serbia');
+INSERT INTO Pais (nombre) VALUES ('Sudáfrica');
+INSERT INTO Pais (nombre) VALUES ('Suecia');
+INSERT INTO Pais (nombre) VALUES ('Suiza');
+INSERT INTO Pais (nombre) VALUES ('Togo');
+INSERT INTO Pais (nombre) VALUES ( 'Trinidad y Tobago');
+INSERT INTO Pais (nombre) VALUES ('Túnez');
+INSERT INTO Pais (nombre) VALUES ('Turquía');
+INSERT INTO Pais (nombre) VALUES ('Ucrania');
+INSERT INTO Pais (nombre) VALUES ( 'Uruguay');
