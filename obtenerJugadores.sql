1bc02e6b06f8364d57a49189cad2bfbece920d39
Carlos Speranza <carlossperanza5@gmail.com>
2018-10-22 15:37:04 -0300

    Subida funcion contar mundiales
    Igual que en el sp, fijense que para ejecutarla tienen que seleccionar alguna de las dos ultimas lineas y apretar f5

--- obtenerJugadores.sql
+++ obtenerJugadores.sql
@@ -6,10 +6,12 @@ CREATE PROCEDURE obtenerJugador(
 	@pasaporte_recibido varchar(20)
 ) AS
 
-SELECT per.nombre_apellido, je.anio_mundial, je.numero_camiseta, je.es_titular
+SELECT per.nombre_apellido AS nombre_completo, pa.nombre as Pais, je.anio_mundial as mundial, je.numero_camiseta , je.es_titular
 
 FROM Juega_En je
 JOIN Persona per ON je.pasaporte_jugador = per.pasaporte
+JOIN Pais pa ON je.codigo_pais = pa.codigo_pais
 WHERE je.pasaporte_jugador = @pasaporte_recibido
 
 EXECUTE obtenerJugador @pasaporte_recibido = 'AA25855B'
+EXECUTE obtenerJugador @pasaporte_recibido = 'salah'
