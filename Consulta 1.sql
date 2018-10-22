5967e80b14cc43ed15a969265b83677b8bdd1d78
Carlos Speranza <carlossperanza5@gmail.com>
2018-10-22 16:44:39 -0300

    Primer consulta que pide del tp

---
+++ Consulta 1.sql
@@ -0,0 +1,5 @@
+SELECT TOP 5 Pais.codigo_pais, Pais.nombre , COUNT(Partido.numero_partido)
+FROM Partido
+JOIN Pais ON (Pais.codigo_pais = Partido.codigo_pais_1 or Pais.codigo_pais = Partido.codigo_pais_2)
+where Partido.codigo_instancia = 6
+GROUP BY Pais.codigo_pais, Pais.nombre
