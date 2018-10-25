8a98c5b97c6867af06f2ceb7a5176c41ab1d8784
Valeria Silvestri <valeria.ayelen@gmail.com>
2018-10-25 15:56:47 -0300

    Se quejaba de que CREATE VIEW deberia ser lo unico en el archivo

--- 7-4-TecnicosMasSelecciones.sql
+++ 7-4-TecnicosMasSelecciones.sql
@@ -6,6 +6,4 @@ JOIN Director_Tecnico ON Equipo.pasaporte_dt = Director_Tecnico.pasaporte_dt
 JOIN Persona ON Director_Tecnico.pasaporte_dt = Persona.pasaporte
 GROUP BY Equipo.pasaporte_dt, Persona.nombre_apellido
 
-SELECT cantMundialesDirigidos.nombre_apellido, cantMundialesDirigidos.cantidad
-FROM cantMundialesDirigidos
-WHERE cantMundialesDirigidos.cantidad = (SELECT MAX(cantMundialesDirigidos.cantidad) FROM cantMundialesDirigidos)
+
